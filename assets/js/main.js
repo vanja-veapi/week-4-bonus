88ba830ca5d23245b3c2fdc987fd944109474c5b
vanja-veapi <hrastivoje@gmail.com>
2021-10-01 13:18:48 +0200

    JS form fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,66 +17,61 @@ window.addEventListener("load", function(){
     let errorPassword = this.document.querySelector("#error-password");
 
     //RegularExpression
-    let regExpNameSurname = /^[A-ZŠĐŽČĆ][a-zšđžčć]{2,12}$/;
     let regExpMail = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
-    let regExpPw = /[\wŠĐČĆŽšđčćž\#\!\$\%\^\&\*\_\ ]{3,30}/;
 
     function checkForm()
     {
-        let obj = [
-            {
-                val: firstName,
-                regularExpression: regExpNameSurname,
-                errorMsg: errorFirst,
-                msg1: "First name cannot be empty",
-                msg2: "Name is not in a good format"
-            },
-            {
-                val: lastName,
-                regularExpression: regExpNameSurname,
-                errorMsg: errorLast,
-                msg1: "Last name cannot be empty",
-                msg2: "Last name is not in a good format"
-            },
-            {
-                val: mail,
-                regularExpression: regExpMail,
-                errorMsg: errorMail,
-                msg1: "Mail cannot be empty",
-                msg2: "Looks like this is not an email"
-            },
-            {
-                val: password,
-                regularExpression: regExpPw,
-                errorMsg: errorPassword,
-                msg1: "Password cannot be empty",
-                msg2: "The Password is not in a good format"
-            }
-        ]
-        console.log(obj);
-        for (let index in obj)
+        if(mail.value === "")
         {
-            let input = obj[index].val;
-            let regEx = obj[index].regularExpression;
-            let errMsg = obj[index].errorMsg;
-
-            if(input.value === "")
+            mail.classList.add("error-img");
+            errorMail.textContent = "Mail cannot be empty";
+        }
+        else
+        {
+            if(!regExpMail.test(mail.value))
             {
-                input.classList.add("error-img");
-                errMsg.textContent = obj[index].msg1;
+                mail.classList.add("error-img");
+                errorMail.textContent = "Looks like this is not an email";
             }
             else
             {
-                if(!regEx.test(input.value))
-                {
-                    input.classList.add("error-img");
-                    errMsg.textContent = obj[index].msg2;
-                }
-                else
-                {
-                    input.classList.remove("error-img");
-                    errMsg.textContent = "";
-                }
+                mail.classList.remove("error-img");
+                errorMail.textContent = "";
+            }
+        }
+
+        let list = [
+            {
+                val: firstName,
+                errorMsg: errorFirst,
+                msg1: "First name cannot be empty"
+            },
+            {
+                val: lastName,
+                errorMsg: errorLast,
+                msg1: "Last name cannot be empty"
+            },
+            {
+                val: password,
+                errorMsg: errorPassword,
+                msg1: "Password cannot be empty"
+            }
+        ]
+
+        for (let index in list)
+        {
+            let input = list[index].val;
+            let errMsg = list[index].errorMsg;
+            
+            if(input.value === "")
+            {
+                input.classList.add("error-img");
+                errMsg.textContent = list[index].msg1;
+            }
+            else
+            {
+                input.classList.remove("error-img");
+                errMsg.textContent = "";
             }
         }
     }
